d6df66cad397213f6fde1c3fbfb4f1275be366cb
Luke C. Orth <lorth001@gmail.com>
2023-01-29 07:40:17 -0600

    Fixing light and dark issue by commenting out code from last commit

--- static/js/katex.js
+++ static/js/katex.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     // provides an escape for dollar sign chars if KaTeX is not desired
-    document.body.innerHTML = document.body.innerHTML.replace(/\\\$/g, '<span>$</span>');
+    //document.body.innerHTML = document.body.innerHTML.replace(/\\\$/g, '<span>$</span>');
 
     renderMathInElement(document.body, {
         delimiters: [
